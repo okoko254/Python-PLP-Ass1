a8163faba13bf2e8e3a4fba6d1241402bd16af78
Rebecca Okoko <okokorebecca39@gmail.com>
2025-02-19 21:01:23 +0300

    Add files via upload

---
+++ Ass1.py
@@ -0,0 +1,21 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+another_list = [50, 60, 70]
+
+my_list.extend(another_list)
+
+last_element = my_list.pop()
+
+my_list.sort()
+
+index_of_30 = my_list.index(30)
+print(index_of_30)
+
+print(my_list)
